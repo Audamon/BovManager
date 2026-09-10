2b7c1b329fba13236b5b3c4cf0f8ff26bb950179
Audamon <joaotamagno@gmail.com>
2026-09-10 20:57:45 -0300

    botão escanear navega para tela de escanear

--- src/components/FloatingButton/FloatingButton.tsx
+++ src/components/FloatingButton/FloatingButton.tsx
@@ -1,10 +1,13 @@
 import {  FAB } from "react-native-paper";
 import QrcodeIcon from "../icons/QrcodeIcon";
 import { useTheme } from "@/hooks/use-theme";
+import { useRouter } from "expo-router";
 const FloatingButton = () => {
+    const router = useRouter();
     const theme = useTheme();
     return (
         <FAB
+        onPress={() => router.navigate("/escanear")}
         label="Escanear"
         color={theme.text}
             icon={({size, color}) =><QrcodeIcon width={size} height={size} color={color} />}
